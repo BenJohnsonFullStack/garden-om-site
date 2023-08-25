b16640daf2d1b5874f368a42df077b7ade870573
Ben Johnson <monk.sound.design@gmail.com>
2023-08-25 19:02:45 -0400

    close icon for Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+
 function Nav(props) {
   const { active, isActive } = props;
 
   return (
     <div className="nav-screen">
-      <div className="nav-bar"></div>
+      <div className="nav-bar">
+        <FontAwesomeIcon icon={faX} style={{ color: "#044418" }} />
+      </div>
       <div className="nav-scrim"></div>
     </div>
   );
