36245327fd6757213abdda6dfee5e2fadcca98f6
Ben Johnson <monk.sound.design@gmail.com>
2023-08-29 22:34:15 -0400

    social links to external

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -23,10 +23,13 @@ function Footer() {
       <div className="social-wrapper">
         <h4>Follow Us on Social Media</h4>
         <div className="social-links">
-          <Link>
+          <Link to="https://www.instagram.com/garden_om_yoga" target="_blank">
             <FontAwesomeIcon icon={faInstagram} className="social-link" />
           </Link>
-          <Link>
+          <Link
+            to="https://www.facebook.com/people/garden-om-yoga/61550656983719"
+            target="_blank"
+          >
             <FontAwesomeIcon icon={faSquareFacebook} className="social-link" />
           </Link>
         </div>
