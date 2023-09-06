c4de16693fbe78959d487597597b07140c286c86
Ben Johnson <monk.sound.design@gmail.com>
2023-09-06 01:43:34 -0400

    environmental PORT variable configured in client proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const PORT = process.env.PORT || 9000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api/subscribers": "http://localhost:9000",
+      "/api/subscribers": `http://localhost:${PORT}`,
     },
   },
   plugins: [react()],
