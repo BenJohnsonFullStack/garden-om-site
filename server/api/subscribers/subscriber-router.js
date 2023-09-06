123148c6395fa0ce362175fd0d2bb14baff98757
Ben Johnson <monk.sound.design@gmail.com>
2023-09-06 02:07:54 -0400

    error handling middleware

--- server/api/subscribers/subscriber-router.js
+++ server/api/subscribers/subscriber-router.js
@@ -3,8 +3,25 @@ const Subscriber = require("./subscriber-model");
 
 const router = express.Router();
 
+//////////// endpoint test //////////////////
 router.get("/", (req, res, next) => {
   res.json("Hello subscribers");
+});
+//////////// endpoint test //////////////////
+
+router.post("/", (req, res, next) => {
+  try {
+    throw new Error("test post");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 module.exports = router;
