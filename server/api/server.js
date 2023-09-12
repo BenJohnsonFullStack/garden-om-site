4059c7b341315c9755f38f95b559918feb21a2b3
Ben Johnson <monk.sound.design@gmail.com>
2023-09-12 00:53:07 -0400

    installed helmet

--- server/api/server.js
+++ server/api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const helmet = require("helmet");
 const subscriberRouter = require("./subscribers/subscriber-router");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 server.use("/api/subscribers", subscriberRouter);
 
 module.exports = server;
