80e2cbbe801d7fd955fa3417f86f2bc6b8fe222d
Ben Johnson <monk.sound.design@gmail.com>
2023-09-15 17:22:25 -0400

    push before deploy attempt

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line no-unused-vars
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
