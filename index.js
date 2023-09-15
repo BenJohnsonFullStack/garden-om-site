82d683f4c4911b741cd036fe62d6d0f5859ae48e
Ben Johnson <monk.sound.design@gmail.com>
2023-09-15 19:55:47 -0400

    refactored server.js into index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 require("dotenv").config();
-const server = require("./api/server");
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+
+const subscriberRouter = require("./api/subscribers/subscriber-router");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use("/api/subscribers", subscriberRouter);
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static(path.join(__dirname, "client/dist")));
+}
 
 const PORT = process.env.PORT || 9000;
 
