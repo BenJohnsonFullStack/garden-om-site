fd28a683dbb4ad9eba7553413b85a2cf9ead5652
Ben Johnson <monk.sound.design@gmail.com>
2023-09-15 19:05:53 -0400

    initial commit

--- data/db-config.js
+++ data/db-config.js
@@ -2,4 +2,6 @@ const knex = require("knex");
 const configs = require("./knexfile.js");
 const environment = process.env.NODE_ENV || "development";
 
+console.log(environment);
+
 module.exports = knex(configs[environment]);
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 require("dotenv").config();
 const server = require("./api/server");
-const path = require("path");
 
 const PORT = process.env.PORT || 9000;
-
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist/index.html"));
-});
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
