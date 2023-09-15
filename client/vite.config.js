0c6d26616ade579da1f6fbba0411d01ae31bb978
Ben Johnson <monk.sound.design@gmail.com>
2023-09-15 14:52:18 -0400

    client side .env config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const PORT = process.env.PORT || 9000;
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api/subscribers": `http://localhost:${PORT}`,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  plugins: [react()],
+    server: {
+      proxy: {
+        "/api/subscribers": `http://localhost:${env.VITE_PORT}`,
+      },
+    },
+    plugins: [react()],
+  };
 });
